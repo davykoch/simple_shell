135083e9942c8df38049cfdcdb7516e37bf3dea5
davykoch <koechdavis24@gmail.com>
2023-09-10 22:13:43 +0300

    header&author files

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#endif /*MAIN_H*/
