d7e87caa21f49c164324579203c78237f162e6f3
kinyuru <kinyuru@localhost.localdomain>
2023-09-11 23:27:05 +0300

    me

--- shell.c
+++ shell.c
@@ -1,2 +1,24 @@
-#include "main.h"
+#include "shell.h"
+/**
+ * main - its the main shell
+ * Return : 0 on success
+ */
+int main()
+{
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
 
+  printf("$");
+  while ((read = getline(&line, &len, stdin)) != -1)
+  {
+    if (read == 0) /* handles EOF */
+    {
+      break;
+    }
+    printf("%s\n", line);
+    printf("$");
+  }
+
+  return (0);
+}
