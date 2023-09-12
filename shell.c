c350230e1df473a8e0bf9792e8e00066e6f1101a
ronaldkinyuru <ronaldkinyuru@gmail.com>
2023-09-12 10:58:13 +0300

    betty

--- shell.c
+++ shell.c
@@ -5,20 +5,20 @@
  */
 int main()
 {
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-  printf("$");
-  while ((read = getline(&line, &len, stdin)) != -1)
-  {
-    if (read == 0) /* handles EOF */
-    {
-      break;
-    }
-    printf("%s\n", line);
-    printf("$");
-  }
+	printf("$");
+	while ((read = getline(&line, &len, stdin)) != -1)
+	{
+		if (read == 0) /* handles EOF */
+		{
+			break;
+		}
+		printf("%s\n", line);
+		printf("$");
+	}
 
-  return (0);
+	return (0);
 }
