7a7dcb954702408e327e268804dd23d4e324a8f5
davykoch <koechdavis24@gmail.com>
2023-09-24 02:46:20 +0300

    setenv

---
+++ setenv.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _setenv - initializes new environment variables
+ * if it does not exist creates a new one
+ * @var: string environment variable value
+ * @val: string environment variable
+ * @info: struct containing potetial arguments
+ * Return: 0 on succcess, - 1 on failure
+ */
+int _setenv(info_t *info, char *var, char *val)
+{
+	char *buf = NULL;
+
+}
+
+/**
+ * _unsetenv - remove environment variable
+ * @info: struct containing potential arguments
+ * @var: string environment variable
+ * Return: 0 on succcess, - 1 on failure
+*/
+int _unsetenv(info_t *info, char *var)
+{
+	
+}
